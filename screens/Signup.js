d14bcab61ea006a0d557dc2e1e54a5116de24862
lam-brian <brianlam408@gmail.com>
2023-12-30 20:24:36 -0800

    setup screens

---
+++ screens/Signup.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+
+const Signup = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Hello from Signup</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default Signup;
