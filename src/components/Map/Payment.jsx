83d05c5d8cba371cc621308aa240653b8a5fe9f4
lam-brian <brianlam408@gmail.com>
2024-01-08 02:13:19 -0800

    adjust spacing on payment component

--- src/components/Map/Payment.jsx
+++ src/components/Map/Payment.jsx
@@ -2,16 +2,16 @@ import { CheckCircleIcon } from "@heroicons/react/24/outline";
 
 const Payment = ({ handleReset }) => {
   return (
-    <div className="flex flex-col items-center text-white">
+    <div className="flex flex-col items-center text-white h-full">
       <CheckCircleIcon className="w-14 text-blue-200 mb-4" />
       <p className="font-bold text-xl mb-5">Payment was successful!</p>
       <p className="mb-4">
         Total trip: <strong>$13.64</strong>
       </p>
-      <p className="text-sm mb-4">
+      <p className="text-sm mb-10">
         You can view your trip history in your Activity.
       </p>
-      <div className="w-full h-px bg-neutrals-100 mb-4"></div>
+      <div className="w-full h-px bg-neutrals-100 mb-auto"></div>
       <button
         onClick={handleReset}
         className="text-base font-semibold bg-blue-200 text-blue-900 py-4 w-full rounded-lg"
